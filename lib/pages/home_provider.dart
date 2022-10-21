716b89af0911a4cb09667fac7c442990174ec924
Semanur Eserler <78433748+Senures@users.noreply.github.com>
2022-10-21 14:53:23 +0300

    home sayfası yapıldı.

---
+++ lib/pages/home_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeProvider extends ChangeNotifier {
+  List img = [
+    "assets/1.png",
+    "assets/2.png",
+    "assets/3.png",
+    "assets/4.png",
+  ];
+}
